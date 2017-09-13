9a67d4acd8842eef9fc781d7770546b1d97b4a9c
Karan Arora <karan.arora@comprotechnologies.com>
2017-09-13 10:29:23 +0530

    GruntFile and gitignore updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,32 @@
+//The wrapper function
+//Every Gruntfile (and gruntplugin) uses this basic format, 
+//and all of your Grunt code must be specified inside this function:
 module.exports = function(grunt){
+
+    //Project and task configuration
+    //Most Grunt tasks rely on configuration data defined in an object passed to the grunt.initConfig method.
     grunt.initConfig({
-        concat:{
-            dist:{
-                src: ['js/1.js', 'js/2.js'],
-                dest: 'build/scripts.js',
-            }
-        },
-        watch: {
-            js: {
-                files: ['js/**/*.js'],
-                tasks: ['jshint'],
-                options: {
-                    spawn: false
-                }
-            }
+        pkg: grunt.file.readJSON('package.json'), //imports the JSON metadata stored in package.json into the grunt config
+        uglify: {
+          options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            //<% %> template strings may reference any config properties
+          },
+          build: {
+            src: 'src/<%= pkg.name %>.js',
+            dest: 'build/<%= pkg.name %>.min.js'
+          }
         }
-    });
-    grunt.loadNpmTasks('grunt-contrib-concat');
+      });
+    
+      //Loading Grunt plugins and tasks
+      // Load the plugin that provides the "uglify" task.      
+      grunt.loadNpmTasks('grunt-contrib-uglify');
+    
+      // Custom tasks
+      // Default task(s).
 
-
-    grunt.loadNpmTasks('grunt-contrib-watch');
-
-    grunt.registerTask('default', ['concat', 'watch', ]);
+      //grunt.task.registerTask(taskName, description, taskFunction)
+      
+      grunt.registerTask('default', ['uglify']);
 };
---
+++ src/gruntproject.js
@@ -0,0 +1,9 @@
+//This function is use to add two numbers
+function sum(a, b){
+	console.log("First Argument is " + a);
+	console.log("Second Argument is " + b);
+	return (a + b);
+}
+
+//Here is the function calling
+console.log(sum(7, 9));
