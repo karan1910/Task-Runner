815cc5dd74c64cdcb4d114f63eae583358616ee7
Karan Arora <karan.arora@comprotechnologies.com>
2017-09-13 10:46:37 +0530

    Gulp added

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+
+/*
+-- TOP LEVEL FUNCTIONS --
+    gulp.task - Define tasks
+    gulp.src - Point tofiles to use
+    gulp.dest - Points to folder to output
+    gulp.watch - Watch files and folders for changes
+--------------------------
+*/
+
+gulp.task('message', function(){
+    console.log("Gulp is running..");
+});
+
+//Copy All HTML files
+gulp.task('copyHTML', function(){
+   gulp.src('src/*.html') 
+    .pipe(gulp.dest('dist'));
+});
